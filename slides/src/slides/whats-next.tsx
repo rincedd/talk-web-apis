2d52946200a4607c2a9caf023c7054793df22de6
Gerd Zschaler <gerd.zschaler@tngtech.com>
2020-06-26 10:45:41 +0200

    Add some intro slides

--- slides/src/slides/whats-next.tsx
+++ slides/src/slides/whats-next.tsx
@@ -11,6 +11,7 @@ export default class WhatsNextSlide extends Component<{clientManager: ClientMana
     return <div>
       <Heading size={3}>What's next?</Heading>
       <UnorderedList>
+        <ListItem><Link href="https://googlechrome.github.io/samples/web-nfc/">WebNFC</Link>: read NFC tags from the browser</ListItem>
         <ListItem><Link href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API">WebBluetooth</Link>: access Bluetooth devices</ListItem>
         <ListItem><Link href="https://www.w3.org/TR/webxr/">WebXR</Link>: access VR/AR devices</ListItem>
         <ListItem><Link href="https://whatwebcando.today">whatwebcando.today</Link></ListItem>
